f9db77525368139c36b4a962a41f1da857eb7826
Shina Oj <shinaoj@gmail.com>
2023-04-24 13:32:54 +0100

    created CustomUser model and migrated

--- jobportal_project/authentication/models.py
+++ jobportal_project/authentication/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    email=models.EmailField(verbose_name='Email Address', max_length=30, unique=True)
+
+    REQUIRED_FIELDS=['user_name', 'first_name', 'last_name']
+    USERNAME_FIELD= 'email'
+
+
+    def get_username(self):
+        return self.email
+
--- jobportal_project/jobportal_project/settings.py
+++ jobportal_project/jobportal_project/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'authentication.apps.AuthenticationConfig',
 ]
 
+AUTH_USER_MODEL= 'authentication.CustomUser'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
